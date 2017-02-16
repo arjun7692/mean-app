be8c1cd2d616317d84d80db61ea235e8d40b2224
Arjun Sawhney <arjun7692@gmail.com>
2017-02-16 23:00:33 +0530

    Initial Commit
    First commit with initial user API and JWT authentication added

---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+  database: 'mongodb://admin:arjun@ds153669.mlab.com:53669/mean2auth',
+  secret: 'mean2auth'
+}
---
+++ routes/users.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
+const config = require('../config/database');
+
+const User = require('../models/user');
+
+// Register
+router.post('/register', (req, res, next) => {
+  let newUser = new User({
+    name: req.body.name,
+    email: req.body.email,
+    username: req.body.username,
+    password: req.body.password
+  });
+
+  User.addUser(newUser, (err, user) => {
+    if(err){
+      res.json({
+        success: false,
+        msg:'Failed to register user',
+        err: err
+      });
+    } else {
+      res.json({
+        success: true,
+        msg:'User Registered'
+      });
+    }
+  });
+});
+
+// Authenticate
+router.post('/authenticate', (req, res, next) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.getUserByUsername(username, (err, user) => {
+    if(err){
+      res.json({
+        success: false,
+        msg:'Failed to authenticate user',
+        err: err
+      });
+    }
+    if(!user){
+      res.json({
+        success: false,
+        msg:'Failed to find user'
+      });
+    }
+
+    User.comparePassword(password, user.password, (err, isMatch) => {
+      if(err) throw err;
+      if(isMatch){
+        const token = jwt.sign(user, config.secret, {
+          expiresIn: 604800 // 1 Week
+        });
+        res.json({
+          success: true,
+          token: 'JWT ' + token,
+          user: {
+            id: user._id,
+            name: user.name,
+            username: user.username,
+            email: user.email
+          }
+        })
+      } else {
+        res.json({
+          success: false,
+          msg:'Wrong Password'
+        });
+      }
+    });
+  });
+});
+
+// Profile
+router.get('/profile', passport.authenticate('jwt', {session:false}), (req, res, next) => {
+  res.json({user: req.user});
+});
+
+module.exports = router;
